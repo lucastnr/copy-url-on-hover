a73db0c3bf5032f213fc8bc7e68efec5e86465bf
Lucas Tenorio <tnrlucas@gmail.com>
2024-01-16 20:47:36 -0300

    Update commands to use CTRL + X and V

--- copylinkaddress.js
+++ copylinkaddress.js
@@ -1,19 +1,25 @@
 function getAbsoluteURL(url) {
     var link = document.createElement("a");
     link.href = url;
-    return (link.protocol + "//" + link.host + link.pathname);
-}
-
-window.onkeydown = function(event) {
-    var hoveredLink = $('a:hover');
-    if (hoveredLink.length) {
-        if (event.key == "x") {
-            navigator.clipboard.writeText(hoveredLink.text())
-        } else if (event.key == "y") {
-            var hoveredHrefAttr = hoveredLink.attr('href');
-            navigator.clipboard.writeText(getAbsoluteURL(hoveredHrefAttr));
-        } else {
-            return;
-        }
-    }
-}
+    return link.protocol + "//" + link.host + link.pathname;
+  }
+  
+  window.onkeydown = function (event) {
+    if (!event.ctrlKey) return;
+    var hoveredLink = $("a:hover");
+  
+    if (!hoveredLink.length) return;
+  
+    const commands = {
+      x: () => {
+        navigator.clipboard.writeText(hoveredLink.text());
+      },
+      v: () => {
+        var hoveredHrefAttr = hoveredLink.attr("href");
+        navigator.clipboard.writeText(getAbsoluteURL(hoveredHrefAttr));
+      },
+    };
+  
+    commands[event.key]?.();
+  };
+  
