5dec01ce7036c61ef3d498fd9f2353869d827d6e
Lucas Tenorio <tnrlucas@gmail.com>
2024-01-16 21:05:38 -0300

    Refactor copylinkaddress.js to use URL object and arrow functions

--- copylinkaddress.js
+++ copylinkaddress.js
@@ -1,25 +1,23 @@
 function getAbsoluteURL(url) {
-    var link = document.createElement("a");
-    link.href = url;
-    return link.protocol + "//" + link.host + link.pathname;
-  }
-  
-  window.onkeydown = function (event) {
-    if (!event.ctrlKey) return;
-    var hoveredLink = $("a:hover");
-  
-    if (!hoveredLink.length) return;
-  
-    const commands = {
-      x: () => {
-        navigator.clipboard.writeText(hoveredLink.text());
-      },
-      v: () => {
-        var hoveredHrefAttr = hoveredLink.attr("href");
-        navigator.clipboard.writeText(getAbsoluteURL(hoveredHrefAttr));
-      },
-    };
-  
-    commands[event.key]?.();
+  const link = new URL(url);
+  return link.toString();
+}
+
+window.onkeydown = (event) => {
+  if (!event.ctrlKey) return;
+  const hoveredLink = $("a:hover");
+
+  if (!hoveredLink.length) return;
+
+  const commands = {
+    x: () => {
+      navigator.clipboard.writeText(hoveredLink.text());
+    },
+    v: () => {
+      const hoveredHrefAttr = hoveredLink.attr("href");
+      navigator.clipboard.writeText(getAbsoluteURL(hoveredHrefAttr));
+    },
   };
-  
+
+  commands[event.key]?.();
+};
